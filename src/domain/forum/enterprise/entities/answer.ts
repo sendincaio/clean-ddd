69279c6090ef00230de7df32bfe334eb0ea4d8f9
Caio Sendin <sendindev@gmail.com>
2024-08-01 21:29:08 -0300

    feat: create entities

---
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -0,0 +1,58 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface AnswerProps {
+    content: string
+    authorId: UniqueEntityId
+    questionId: UniqueEntityId
+    createdAt: Date
+    updatedAt?: Date
+}
+
+export class Answer extends Entity<AnswerProps> {
+    get content() {
+        return this.props.content
+    }
+
+    set content(content: string) {
+        this.props.content = content
+        this.touch()
+    }
+
+    get authorId() {
+        return this.props.authorId
+    }
+
+    get questionId() {
+        return this.props.questionId
+    }
+
+    get createdAt() {
+        return this.props.createdAt
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get excerpt() {
+        return this.content.substring(0, 120).trim().concat('...')
+    }
+
+    private touch() {
+        this.props.updatedAt = new Date()
+    }
+
+    static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId) {
+        const answer = new Answer(
+            {
+                ...props,
+                createdAt: props.createdAt ?? new Date(),
+            },
+            id,
+        )
+
+        return answer
+    }
+}
